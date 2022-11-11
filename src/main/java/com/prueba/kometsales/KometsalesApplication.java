161029a31fdf724bcb88357a1f50186fa5a24a28
Santiago Sandoval <casaba3131@gmail.com>
2022-11-11 11:36:28 -0600

    Java structure is configured with Spring boot of the project

---
+++ src/main/java/com/prueba/kometsales/KometsalesApplication.java
@@ -0,0 +1,14 @@
+package com.prueba.kometsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KometsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KometsalesApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/prueba/kometsales/KometsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.kometsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KometsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
